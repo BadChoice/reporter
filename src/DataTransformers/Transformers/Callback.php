f7d203ed06aa58909640c2b8aff39f74bb6eae98
Pau <pau.benet.prat@gmail.com>
2017-09-18 16:58:58 +0200

    link content deleted and adapted Link class to get content

--- src/DataTransformers/Transformers/Callback.php
+++ src/DataTransformers/Transformers/Callback.php
@@ -4,11 +4,9 @@ namespace BadChoice\Reports\DataTransformers\Transformers;
 
 use BadChoice\Reports\DataTransformers\TransformsRowInterface;
 
-class Callback implements TransformsRowInterface
-{
+class Callback implements TransformsRowInterface {
+
     public function transformRow($field, $row, $value, $transformData){
         return $transformData($value);
-        /*$callback = $transformData; //TODO
-        return $row->$callback();*/
     }
 }
