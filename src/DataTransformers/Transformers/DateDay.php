22cb62ac85c0acb90a35f20d899a0e85f406c288
Pau <pau.benet.prat@gmail.com>
2017-09-18 11:33:00 +0200

    Add empty string -- when date is null

--- src/DataTransformers/Transformers/DateDay.php
+++ src/DataTransformers/Transformers/DateDay.php
@@ -5,9 +5,10 @@ namespace BadChoice\Reports\DataTransformers\Transformers;
 use BadChoice\Reports\DataTransformers\TransformsValueInterface;
 use Carbon\Carbon;
 
-class DateDay implements TransformsValueInterface
-{
-    public function transform($value){
-        return Carbon::parse($value)->timezone( auth()->user()->timezone )->day;
+class DateDay implements TransformsValueInterface {
+
+    public function transform( $value ) {
+        if ( ! $value ) return "--";
+        return Carbon::parse( $value )->timezone( auth()->user()->timezone )->day;
     }
 }
