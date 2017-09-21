8e528ea9095e30dc59eab1158482c483ed29a94c
Pau <pau.benet.prat@gmail.com>
2017-09-21 09:26:27 +0200

    fix icon display on links

--- src/DataTransformers/Transformers/LinkId.php
+++ src/DataTransformers/Transformers/LinkId.php
@@ -7,18 +7,17 @@ use BadChoice\Reports\DataTransformers\TransformsRowInterface;
 class LinkId implements TransformsRowInterface {
 
     public function transformRow($field, $row, $value, $transformData){
-        if( is_array($transformData) ){
+        $class = "";
+        $text  = $value;
+        $link  = str_replace("{id}", $row['id'], $transformData);
+        if ( is_array($transformData) ) {
             $class = $transformData['class'] ?? "";
             $text  = $transformData['text']  ?? $value;
             $link  = str_replace("{id}", $row['id'], $transformData['url']);
             if( isset($transformData['icon']) ){
-                return "<a class='{$class}' href='". url($link)."' style='font-size:15px'> ". icon($row['icon']) . "</a>";
+                return "<a class='{$class}' href='" . url($link) . "' style='font-size:15px'> " . icon($transformData['icon']) . "</a>";
             }
-        }else{
-            $class = "";
-            $text  = $value;
-            $link  = str_replace("{id}", $row['id'], $transformData);
         }
-        return link_to($link, $text, ["class" => $class] );
+        return link_to($link, $text, compact("class"));
     }
 }
