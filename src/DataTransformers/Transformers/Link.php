7968272f5b6add8fe62cc0f068f7e150ba932af2
Pau <pau.benet.prat@gmail.com>
2017-09-27 19:56:59 +0200

    use regex to find and replace custom Ids

--- src/DataTransformers/Transformers/Link.php
+++ src/DataTransformers/Transformers/Link.php
@@ -5,22 +5,34 @@ namespace BadChoice\Reports\DataTransformers\Transformers;
 use BadChoice\Reports\DataTransformers\TransformsRowInterface;
 
 class Link implements TransformsRowInterface {
+
     public function transformRow($field, $row, $value, $transformData) {
         $class = "";
         $text  = $value;
-        $link  = str_replace("{" . $field . "}", $value, $transformData);
-        if( is_array($transformData) ) {
+        $link  = $this->parseLink($row, $transformData);
+        if ( is_array($transformData) ) {
             $class = $transformData['class'] ?? "";
             $text  = ($transformData['content'] ?? "") . ($transformData['text'] ?? $value);
-            if ( isset( $transformData["customId"] ) ) {
-                $field = $transformData["customId"] ;
-                $value = $row[$transformData["customId"]];
-            }
-            $link = str_replace("{" . $field . "}", $value, $transformData['url']);
             if ( isset($transformData['icon']) ) {
                 return "<a class='{$class}' href='" . url($link) . "' style='font-size:15px'> " . icon($transformData['icon']) . "</a>";
             }
         }
         return link_to($link, $text, compact("class"));
+    }
+
+    /**
+     * @param $row
+     * @param $link
+     * @return mixed
+     */
+    private function parseLink($row, $link) {
+        $link = is_array($link) ? $link['url'] : $link;
+        $matches = null;
+        $result = preg_match("/{([a-z,_,-]*)}/", $link, $matches);
+        while ( $result ) {
+            $link = str_replace($matches[0], $row[$matches[1]], $link);
+            $result = preg_match("/{([a-z,_,-]*)}/", $link, $matches);
+        }
+        return $link;
     }
 }
