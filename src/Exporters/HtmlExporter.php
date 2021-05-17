8a93e2194513f6a75b2111ed088c16f7ccdb916b
aleixgilaguilar <aleix.gil@revo.works>
2021-05-17 18:18:35 +0200

    Correction.

--- src/Exporters/HtmlExporter.php
+++ src/Exporters/HtmlExporter.php
@@ -58,10 +58,8 @@ class HtmlExporter extends BaseExporter
                 $value = $field->getValue($row);
                 if ($field->isNumeric())  {
                     $classes = "{$classes} text-right";
-                } else {
-                    if ( !$field->transformation ) {
-                        $value = htmlentities($value);
-                    }
+                } elseif ( !$field->transformation ) {
+                    $value = htmlentities($value);
                 }
                 $this->output .= "<td class='{$classes}'>{$value}</td>";
             }
